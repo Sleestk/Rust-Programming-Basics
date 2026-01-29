6a3b9027c329e6217189b5af95bc996230d7d01c
j-ble <blemasterjacob@gmail.com>
2026-01-29 12:46:11 -0700

    Array

---
+++ hello_rust/examples/array.rs
@@ -0,0 +1,29 @@
+#![allow(unused)]
+
+// Array - collection of elements with length known at compile time
+// Slice - collection of elements with length now known at compile time
+fn main() {
+    // Array
+    let arr: [u32; 3]  = [1, 2, 3];
+    println!("arr[0]: {}", arr[0]);
+
+    // Write
+    let mut arr: [u32; 3]  = [1, 2, 3];
+    arr[1] = 99;
+
+    let arr: [u32; 10] = [0; 10];
+    println!("arr: {:?}", arr);
+
+    // Slice
+    let nums: [i32; 10] = [-1, 1, -2, 2, -3, 3, -4, 4, -5, 5];
+
+    // First 3
+    let s: &[i32] = &nums[0..3];
+    println!("First 3: {:?}", s);
+    // Last 3 (indexes = 7, 8, 9)
+    let s: &[i32] = &nums[7..];
+    println!("Last 3: {:?}", s);
+    // Middle 4 (indexes = 3, 4, 5, 6)
+    let s: &[i32] = &nums[3..7];
+    println!("mid 4: {:?}", s)
+}
