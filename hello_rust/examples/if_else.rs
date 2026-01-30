3867a4418f5dd07ffc4fee72e220f3485adc948c
j-ble <blemasterjacob@gmail.com>
2026-01-29 20:27:13 -0700

    If Else

---
+++ hello_rust/examples/if_else.rs
@@ -0,0 +1,18 @@
+#![allow(unused)]
+
+// If / else
+fn main() {
+    let x: i32 = 10;
+
+    let z: i32 = if x > 0 {
+        println!("x > 0");
+        1
+    } else if x < 0 {
+        println!("x < 0");
+        -1
+    } else {
+        println!("x = 0");
+        0
+    };
+    println!("z: {}", z);
+}
