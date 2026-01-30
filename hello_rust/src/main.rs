9fc6255b0df6162a5dd007ad93753251ba1a1bd1
j-ble <blemasterjacob@gmail.com>
2026-01-30 12:49:31 -0700

    Borrow Rules Exercises

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -4,5 +4,16 @@
 // cargo test
 
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("Rust");
+    let s1 = &s;
+    let s2 = &s;
+    println!("{s1}, {s2}");
+
+    let mut s = String::from("Rust");
+    let s1 = &mut s;
+    // let s2 = &mut s;
+    println!("{s1}");
+
+    let s = String::from("Rust");
+    println!("{s}");
 }
