59f710073eb595625e78ec043d7656b046921b40
j-ble <blemasterjacob@gmail.com>
2026-01-30 10:42:53 -0700

    If Let

---
+++ hello_rust/examples/if_let.rs
@@ -0,0 +1,14 @@
+#![allow(unused)]
+
+// If Let
+fn main () {
+    let x: Option<i32> = Some(9);
+    match x {
+        Some(val) => println!("Option is {val}"),
+        None => {}
+    }
+
+    if let Some(val) = x {
+        println!("Option is {val}");
+    }
+}
