c82313bff4947c563e4bc74c94c77328108ad058
j-ble <blemasterjacob@gmail.com>
2026-02-01 15:33:53 -0700

    Iteratror Adaptors Exercises

--- hello_rust/examples/iter.rs
+++ hello_rust/examples/iter.rs
@@ -2,14 +2,19 @@
 
 use::std::collections::HashMap;
 
+fn filter_non_zero(v: Vec<i32>) -> Vec<i32> {
+    v.iter().filter(|x: &&i32| **x > 0).map(|x: &i32| x - 3).collect()
+}
+
 // Iterators
 // - map, filter, collect
 fn main() {
-    let vals: Vec<u32> = vec![1,2,3,4,5];
-    //&u32
-    // map - f(x: &u32) -> u32
-    let v2: Vec<u32> = vals.iter().map(|x| x + 1).collect();
-    println!("v2: {:?}", v2);
+    let vals: Vec<i32> = vec![1,2,3,4,5];
+    println!("filter non zero: {:?}", filter_non_zero(vals));
+    // //&u32
+    // // map - f(x: &u32) -> u32
+    // let v2: Vec<u32> = vals.iter().map(|x| x + 1).collect();
+    // println!("v2: {:?}", v2);
 
     //collect
     let vals: Vec<(&str, u32)> = vec![("a", 1), ("b", 2), ("c", 3)];
@@ -24,10 +29,10 @@ fn main() {
     // &T
     let v: Vec<u32> = vals.iter().filter(|x: &&u32| **x <= 3).map(|x: &u32| x + 1).collect();
     println!("filter -> map: {:?}", v);
-    
     // into_iter - iterate T example
     // let v: Vec<u32> = vals.into_iter().filter(|x: &u32| *x <= 3).map(|x: u32| x + 1).collect();
     // println!("filter -> map: {:?}", v);
+
 
     // let vals: Vec<u32> = vec![1,2,3,4,5];
     // // into_iter - iterate T
