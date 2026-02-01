4fcab820cda6c8e2f0bc522cddc423a3ec4af1a2
j-ble <blemasterjacob@gmail.com>
2026-02-01 15:20:45 -0700

    Iterator Adaptors

--- hello_rust/examples/iter.rs
+++ hello_rust/examples/iter.rs
@@ -1,19 +1,46 @@
 #![allow(unused)]
 
+use::std::collections::HashMap;
+
 // Iterators
+// - map, filter, collect
 fn main() {
     let vals: Vec<u32> = vec![1,2,3,4,5];
-    // into_iter - iterate T
-    // iter - iterate &T
-    // iter_mut - iterate &mut T
+    //&u32
+    // map - f(x: &u32) -> u32
+    let v2: Vec<u32> = vals.iter().map(|x| x + 1).collect();
+    println!("v2: {:?}", v2);
 
-    for v: u32 in vals.into_iter() {
-        // 
-    }
-    for v: &u32 in vals.iter() {
-        // 
-    }
-    for v: &mut u32 in vals.iter_mut() {
-        // 
-    }
+    //collect
+    let vals: Vec<(&str, u32)> = vec![("a", 1), ("b", 2), ("c", 3)];
+    let v: Vec<(String, u32)> = vals.iter().map(|v| (v.0.to_string(), v. 1 + 1)).collect();
+    println!("vec: {:?}", v);
+
+    let v: HashMap<String, u32> = vals.iter().map(|v| (v.0.to_string(), v.1 + 1)).collect();
+    println!("hashmap: {:?}", v);
+
+    // Chaining filter and map
+    let vals: Vec<u32> = vec![1,2,3,4,5];
+    // &T
+    let v: Vec<u32> = vals.iter().filter(|x: &&u32| **x <= 3).map(|x: &u32| x + 1).collect();
+    println!("filter -> map: {:?}", v);
+    
+    // into_iter - iterate T example
+    // let v: Vec<u32> = vals.into_iter().filter(|x: &u32| *x <= 3).map(|x: u32| x + 1).collect();
+    // println!("filter -> map: {:?}", v);
+
+    // let vals: Vec<u32> = vec![1,2,3,4,5];
+    // // into_iter - iterate T
+    // // iter - iterate &T
+    // // iter_mut - iterate &mut T
+
+    // for v: u32 in vals.into_iter() {
+    //     // 
+    // }
+    // for v: &u32 in vals.iter() {
+    //     // 
+    // }
+    // for v: &mut u32 in vals.iter_mut() {
+    //     // 
+    // }
 }
