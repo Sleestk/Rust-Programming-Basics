3995211369c4610a0012c167353d5a52a1e63161
j-ble <blemasterjacob@gmail.com>
2026-02-25 18:19:40 -0700

    Fix crashing using Async/Await

--- hello_rust/examples/async.rs
+++ hello_rust/examples/async.rs
@@ -1,5 +1,7 @@
 #![allow(unused)]
 
+use tokio::time::Duration;
+
 struct Tomato;
 struct Lettuce;
 struct Cheese;
@@ -68,8 +70,27 @@ async fn make_hamburger() -> Hamburger {
     }
 }
 
+// When to use threads vs async / await?
+// Need to parallelize computation -> use threads
+// Need to parallelize waiting time -> use async / await
+
 #[tokio::main]
 async fn main() {
     let fut = make_hamburger();
     fut.await;
+
+    // Spawning to many threads can crash this program (OS thread and memory limits)
+    let mut handles = vec![];
+    for i in 0..1000000 {
+        let fut = async move {
+            tokio::time::sleep(Duration::from_millis(100)).await;
+            println!("{i}: 🍔 is ready");
+        };
+
+        let handler: tokio::task::JoinHandle<()> = tokio::task::spawn(fut);
+        handles.push(handler);
+    }
+    for h in handles {
+        h.await.unwrap();
+    }
 }
